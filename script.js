be3c06da46abadc9f887b8e1d6199ac19c7483ac
sudhirM45 <sudhirmhamane@908.in>
2024-11-09 09:11:26 +0530

    changes done

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
  const topButton = document.getElementById("backToTop");
 
   window.onscroll = function() {
-    if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
+    if (document.body.scrollTop > 2200 || document.documentElement.scrollTop > 2200) {
       topButton.style.display = "block";
     } else {
       topButton.style.display = "none";
@@ -19,3 +20,24 @@ document.addEventListener('DOMContentLoaded', () => {
         behavior: "smooth" // Smooth scrolling effect
     });
   });
+
+  document.querySelector('.intro-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    const profileSection = document.querySelector('.profile-section');
+    
+    // Responsive offset based on screen width
+    let offset;
+    if (window.innerWidth <= 480) {
+        offset = -120; // Small screen offset
+    } else if (window.innerWidth <= 768) {
+        offset = -40; // Tablet offset
+    } else {
+        offset = -160; // Desktop offset
+    }
+
+    window.scrollTo({
+        top: profileSection.offsetTop + offset,
+        behavior: 'smooth'
+    });
+});
+
